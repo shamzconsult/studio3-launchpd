280a701a84f73173c1b13535bbc732b40f6cb13d
eedrisofficial <eedrisharuna3@gmail.com>
2025-02-21 03:00:50 -0500

    updated SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,29 @@ import { Footer } from "@/components/atoms/Footer";
 import { openSans } from "@/const/font";
 
 export const metadata: Metadata = {
-  title: "Edutech",
-  description: "",
+  title: "Studio3 Launchpad",
+  description: "Online tech training programs",
+  openGraph: {
+    images: [
+      "https://res.cloudinary.com/dcgghkk7q/image/upload/v1740037804/HR_LOGO1_4_3_nfapse.png",
+    ],
+  },
+  keywords: [
+    "Online tech training programs",
+    "Launch your tech career",
+    "Tech training program",
+    "Learn in demand tech skills",
+    "Best online training platforms",
+    "Digital skills for young professionals",
+    "Digital skills bootcamp",
+    "Learn software development online",
+    "Data analytics training course",
+    "Product management certification",
+    "Web3 development courses",
+    "UI/UX design training",
+    "Affordable tech education",
+    "Cohort-based learning programs",
+  ],
 };
 
 export default function RootLayout({
